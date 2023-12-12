355a5d24f4be96e210abe08d026390d8305de151
Gia Phu Huynh <huynhgiaphu3004@gmail.com>
2023-12-12 10:50:09 +0700

    added algorithms

---
+++ book/bstrGen/bsrt.py
@@ -0,0 +1,24 @@
+import os
+
+directory = fr"{os.getcwd()}\\book\\bstrGen\\"
+InputFile = "bstr.inp"
+OutputFile = "bstr.out"
+
+x = ""
+
+with open(directory+InputFile, 'r') as f:
+    n = i = int(f.readline())
+    x = ["0"] * n
+    
+with open(directory+OutputFile, 'w') as f:
+    binary = ''.join(x[::-1])
+    f.write(binary+"\n")
+    while x != ["1"]*n:
+        for i in range(n):
+            if x[i] == "0":
+                x[i] = "1"
+                break
+            else:
+                x[i] = "0"
+        binary = ''.join(x[::-1])
+        f.write(binary+"\n")
